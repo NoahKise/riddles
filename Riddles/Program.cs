038ec62cc5be6474a5a375defcf24be348a08776
noahkise <noah@kisefamily.com>
2024-01-16 15:52:28 -0800

    fully functional MVP
    Co-authored-by: Henry Oberholtzer <henryoberholtzer@gmail.com>

--- Riddles/Program.cs
+++ Riddles/Program.cs
@@ -7,6 +7,7 @@ class Program
 {
     static void Main()
     {
+        ShowSpinner();
       Riddle first = new("What has a trunk but no key?", "elephant");
       Riddle second = new("There are three men an a boat with a pack of cigarettes and no matches. How did they manage to smoke?", "lighter");
       Riddle third = new("What room can none enter", "mushroom");
@@ -15,6 +16,8 @@ class Program
     }
     static void RunGame()
         {
+        // Console.BackgroundColor = ConsoleColor.Blue;
+        // Console.ForegroundColor = ConsoleColor.White;
         bool playAgain = true;
         while (playAgain)
         {
@@ -33,6 +36,11 @@ class Program
                 {
                     Console.WriteLine("--------------------------------------------------");
                     Console.WriteLine("~~Ah, a lucky guess~~");
+                    if (i == 2) 
+                    {
+                      playAgain = false;
+                      WinGame();
+                    }
                 }
                 else
                 {
@@ -42,16 +50,57 @@ class Program
                     string userInput = Console.ReadLine().ToLower();
                     if (userInput == "y")
                     {
-                        break;
+                      playAgain = true;
+                      break;
                     }
                     else
                     {
                         playAgain = false;
+                        Console.WriteLine("~~Thanks for playing goodbye!~~");
                         break;
                     }
                 }
             }
         }
-        Console.WriteLine("~~Thanks for playing goodbye!~~");
+        
+    }
+
+    static void ShowSpinner()
+    {
+        int counter = 0;
+        for (int i = 0; i < 50; i++)
+        {
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("|"); break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            counter++;
+            Thread.Sleep(100);
+        }
+    }
+
+    static void WinGame() {
+      Console.WriteLine("Congratulations... you have answered my riddles...\n");
+      Console.WriteLine("Your prize... if you should choose to collect it... awaits\n");
+      Console.WriteLine("Collect your prize (y)");
+      Console.WriteLine("Forgo worldly possesions (any key)");
+      string userInput = Console.ReadLine().ToLower();
+      if (userInput == "y") {
+        Console.WriteLine("Your prize... is here...");
+        Console.WriteLine(Jackal.TreasureChest);
+        Console.WriteLine("Come closer...(press any key to continue)");
+        Console.ReadKey();
+        Console.WriteLine("HAH!\nYou're still attached to the world of mortal posessions...\nPlay my game again see if you have learned anything...");
+        RunGame();
+      } 
+      else 
+      {
+        Console.WriteLine("Well, I'll be.\n");
+        Console.WriteLine("Your soul has passed my judgement, you may exit my application.");
+      }
     }
 }
